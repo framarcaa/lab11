8d54c95691b33176b994e0bc1033c1b48328c9f1
Francesco Marcatelli <francesco.marcatelli@studio.unibo.it>
2023-12-05 15:10:33 +0100

    Cheackpoint 112, did it enum "fields"

--- 112-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
+++ 112-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
@@ -6,7 +6,9 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.Toolkit;
+import java.util.Arrays;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -33,12 +35,25 @@ import javax.swing.JTextArea;
 public final class LambdaFilter extends JFrame {
 
     private static final long serialVersionUID = 1760990730218643730L;
+    private static final String ANY_NON_WORD = "(\\s|\\p{Punct})+";
 
     private enum Command {
         /**
          * Commands.
          */
-        IDENTITY("No modifications", Function.identity());
+        IDENTITY("No modifications", Function.identity()),
+        LOWER("Lowercase", String::toLowerCase),
+        COUNT_CHARS("Count chars", s -> Integer.toString(s.length())),
+        COUNT_LINES("Count lines", s -> Long.toString(s.chars().filter(c -> c == '\n').count() + 1)),
+        SORTED_WORDS("Sort word", s -> Arrays.stream(s.split(ANY_NON_WORD)).sorted().collect(Collectors.joining("\n"))),
+        COUNT_WORD("Count words", s -> 
+            Arrays.stream(s.split(ANY_NON_WORD))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .map(e -> e.getKey() + " -> " + e.getValue())
+                .collect(Collectors.joining("\n"))
+        );
+        
 
         private final String commandName;
         private final Function<String, String> fun;
